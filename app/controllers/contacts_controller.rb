7dd6b0ecbfa75d73a692233612995c0eca6711b0
sienko-ror <przemek@przemek-debian>
2014-03-31 22:33:45 +0200

    sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,7 @@ if @contact.valid?
 @contact.update_spreadsheet
 
 # TODO send message
+UserMailer.contact_email(@contact).deliver
 flash[:notice] = "Message sent from #{@contact.name}."
 redirect_to root_path
 else
